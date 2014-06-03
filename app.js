a3e8fbc42463e5df4ae5ef759de931744ada7138
root <root@AY140602192024631d08Z.(none)>
2014-06-04 00:19:40 +0800

    modified add to app

--- app.js
+++ app.js
@@ -8,25 +8,25 @@ app.listen(port)
 
 console.log('imooc started on port '+port)
 
-add.get('/',function(req,res){
+app.get('/',function(req,res){
 	res.render('detail',{
 		title:'imooc 首页'
 	})
 })
 
-add.get('/movie/:id',function(req,res){
+app.get('/movie/:id',function(req,res){
 	res.render('detail',{
 		title:'imooc 详情页'
 	})
 })
 
-add.get('/admin/movie',function(req,res){
+app.get('/admin/movie',function(req,res){
 	res.render('detail',{
 		title:'imooc 后台录入页面'
 	})
 })
 
-add.get('/admin/list',function(req,res){
+app.get('/admin/list',function(req,res){
 	res.render('detail',{
 		title:'imooc 列表页'
 	})
