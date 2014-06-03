aaed64b4f0d6e04e06cab73d6955f58675e000c7
congyuandong <congyuandong@gmail.com>
2014-06-04 01:50:47 +0800

    modified summary

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.get('/',function(req,res){
 })
 
 app.get('/movie/:id',function(req,res){
-	res.render('/movie/:id',{
+	res.render('detail',{
 		title:'imooc 详情页',
 		movie:{
 			doctor:'何塞',
@@ -55,13 +55,13 @@ app.get('/movie/:id',function(req,res){
 			poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
 			language:'英语',
 			flash:'http://player.youku.com/player.php/sid/XNjM3Njc3MTY4/v.swf',
-			summary:'哈哈哈'
+			summary:'2028年，专事军火开发的机器人公司Omni Corp.生产了大量装备精良的机械战警，他们被投入到维和和惩治犯罪等行动中，取得显著的效果。罪犯横行的底特律市，嫉恶如仇、正义感十足的警察亚历克斯·墨菲（乔尔·金纳曼 饰）遭到仇家暗算，身体受到毁灭性破坏。借助于Omni公司天才博士丹尼特·诺顿（加里·奥德曼 饰）最前沿的技术，墨菲以机械战警的形态复活。数轮严格的测试表明，墨菲足以承担起维护社会治安的重任，他的口碑在民众中直线飙升，而墨菲的妻子克拉拉（艾比·考尼什 饰）和儿子大卫却再难从他身上感觉亲人的温暖。 '
 		}
 	})
 })
 
 app.get('/admin/movie',function(req,res){
-	res.render('/admin/movie',{
+	res.render('/admin',{
 		title:'imooc 后台录入页面',
 		movie:{
 			doctor:'',
@@ -88,7 +88,7 @@ app.get('/admin/list',function(req,res){
 			year:'2014',
 			language:'英语',
 			flash:'http://player.youku.com/player.php/sid/XNjM3Njc3MTY4/v.swf',
-			summary:'你好啊'
+			summary:'2028年，专事军火开发的机器人公司Omni Corp.生产了大量装备精良的机械战警，他们被投入到维和和惩治犯罪等行动中，取得显著的效果。罪犯横行的底特律市，嫉恶如仇、正义感十足的警察亚历克斯·墨菲（乔尔·金纳曼 饰）遭到仇家暗算，身体受到毁灭性破坏。借助于Omni公司天才博士丹尼特·诺顿（加里·奥德曼 饰）最前沿的技术，墨菲以机械战警的形态复活。数轮严格的测试表明，墨菲足以承担起维护社会治安的重任，他的口碑在民众中直线飙升，而墨菲的妻子克拉拉（艾比·考尼什 饰）和儿子大卫却再难从他身上感觉亲人的温暖。 '
 		}]
 	})
 })
